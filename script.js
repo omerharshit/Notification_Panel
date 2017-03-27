bfb2d7ea5e6686688c473bf0dcbdf6ad6e62fc89
Harshit Omer <omerharshit1997@gmail.com>
2017-03-28 00:53:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+Copyright (c) 2015 Harshit Omer
+(http://www.harshitomer.me/)
+All Rights Reserved.
+*/
+
+var dntrigger = document.getElementById('dntrigger');
+
+dntrigger.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      if (!window.Notification) {
+            alert("Sorry, Notification Not supported in this Browser!");
+      } else {
+            if (Notification.permission === 'default') {
+                  Notification.requestPermission(function(p) {
+                        if (p === 'denied')
+                              alert('You have denied Notification from Team Abhivyakti');
+                        else {
+                              notify = new Notification('New Notifications!', {
+                                    body: 'Simple Desktop Notification by Harshit Omer!"',
+                                    icon: 'https://www.approveme.com/wp-content/uploads/edd/2014/07/document-activity-notifications.png'
+                              });
+                        }
+                  });
+            } else {
+                  notify = new Notification('New Notifications!', {
+                        body: 'Simple Desktop Notification by Harshit Omer!"',
+                        icon: 'https://www.approveme.com/wp-content/uploads/edd/2014/07/document-activity-notifications.png'
+                        // You Can give image Link to change notification Icon.
+                  });
+            }
+      }
+});
