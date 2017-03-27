b080901099f900b3cbacbac31887b9793d700e34
Harshit Omer <omerharshit1997@gmail.com>
2017-03-28 00:46:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Desktop Notifications Using Javascript</title>
+    <link href='https://fonts.googleapis.com/css?family=Lobster+Two:400,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/Projects Javascript/Notification software/CSS/style.css">
+    <link rel="stylesheet" href="/Projects Javascript/Notification software/CSS/stars.css">  
+  </head>
+  <body style="overflow:hidden;">
+  
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+    
+    <div class="main-wrapper">
+      <h2>A Pen by Harshit Omer.</h2>
+      <br>
+      <p>Simply click on the Yellow Bordered button on the bottom to trigger Notifications!</p>
+      <br>
+      <h3>Hope you like it!!</h3><br>
+      <center><button id="dntrigger">Click</button></center>
+    </div>
+    <script src="/Projects Javascript/Notification software/javascript/script.js"></script>
+  </body>
+</html>
